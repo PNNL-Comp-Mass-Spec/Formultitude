b61d5b2d48ab41c8baba6eaf12ae908ff8525be8
alchemistmatt <matthew.monroe@pnnl.gov>
2018-12-12 17:22:59 -0800

    Update source directory in distribution batch file

--- CIA/bin/Distribute_Formularity.bat
+++ CIA/bin/Distribute_Formularity.bat
@@ -1,6 +1,6 @@
 @echo on
 
-xcopy NoExcel\x64\Debug\*.* C:\DMS_Programs\Formularity /D /Y
-xcopy NoExcel\x64\Debug\*.* \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\Formularity /D /Y
+xcopy x64\Debug\*.* C:\DMS_Programs\Formularity /D /Y
+xcopy x64\Debug\*.* \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\Formularity /D /Y
 
 pause
