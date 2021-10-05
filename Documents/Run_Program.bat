12a50b5a8bd85db178c53697ddb08d95991662cc
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-04 20:14:06 -0700

    Update batch file

--- Documents/Run_Program.bat
+++ Documents/Run_Program.bat
@@ -1,1 +1,21 @@
-..\CIA\bin\x64\Debug\CIA.exe cia TestData\data_4_formularity.txt TestData\param_4_formularity.xml C:\DBs\WHOI_CIA_DB_2016_11_21.bin
+@echo off
+
+set ExePath=CIA.exe
+
+if exist %ExePath% goto DoWork
+if exist ..\%ExePath% set ExePath=..\%ExePath% && goto DoWork
+if exist ..\CIA\bin\x64\Debug\%ExePath% set ExePath=..\CIA\bin\x64\Debug\%ExePath% && goto DoWork
+
+echo Executable not found: %ExePath%
+goto Done
+
+:DoWork
+echo.
+echo Procesing with %ExePath%
+echo.
+
+%ExePath% cia data_4_formularity.txt param_4_formularity.xml ..\Data\CIA_DB\WHOI_CIA_DB_2016_11_21.bin
+
+:Done
+
+pause
