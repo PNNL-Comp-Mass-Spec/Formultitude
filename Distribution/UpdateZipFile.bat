e9d7fe5b00748b67e5d30fd9cb4f18abf045c3da
alchemistmatt <matthew.monroe@pnnl.gov>
2023-03-17 11:29:24 -0700

    Update batch file to also update FileReader.exe

--- Distribution/UpdateZipFile.bat
+++ Distribution/UpdateZipFile.bat
@@ -1,3 +1,3 @@
-"C:\Program Files\7-Zip\7z.exe" a Formularity_Program.zip ..\CIA\bin\x64\Debug\CIA.exe ..\CIA\bin\x64\Debug\CIA.pdb ..\CiaUi\bin\x64\Debug\Formularity.exe ..\CiaUi\bin\x64\Debug\Formularity.pdb ..\CiaUi\bin\x64\Debug\DefaultParameters.xml ..\CiaUi\bin\x64\Debug\TestFSDBSearch.exe ..\Documents\Isotope.inf ..\Readme.md
+"C:\Program Files\7-Zip\7z.exe" a Formularity_Program.zip ..\CIA\bin\x64\Debug\CIA.exe ..\CIA\bin\x64\Debug\CIA.pdb ..\CIA\bin\x64\Debug\FileReader.exe ..\CIA\bin\x64\Debug\FileReader.pdb ..\CiaUi\bin\x64\Debug\Formularity.exe ..\CiaUi\bin\x64\Debug\Formularity.pdb ..\CiaUi\bin\x64\Debug\DefaultParameters.xml ..\CiaUi\bin\x64\Debug\TestFSDBSearch.exe ..\Documents\Isotope.inf ..\Readme.md
 
 pause
