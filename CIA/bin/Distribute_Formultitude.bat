310ffdf53c3a70059801c52225d49cb4ed83233c
alchemistmatt <matthew.monroe@pnnl.gov>
2025-10-22 18:23:26 -0700

    Update batch distribution file

--- CIA/bin/Distribute_Formultitude.bat
+++ CIA/bin/Distribute_Formultitude.bat
@@ -1,6 +1,6 @@
 @echo on
 
 xcopy x64\Debug\*.* C:\DMS_Programs\Formultitude /D /Y
-xcopy x64\Debug\*.* \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\Formultitude /D /Y
+xcopy x64\Debug\*.* \\Proto-3\DMS_Programs_Dist\AnalysisToolManagerDistribution\Formultitude /D /Y
 
 pause
